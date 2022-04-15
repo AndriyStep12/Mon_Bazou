94aaf566b68e412af1eb819e30adf209ba34621a
Andriy Stepaniak <85013328+AndriyStep12@users.noreply.github.com>
2022-04-15 11:55:29 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+let developer = {
+            name: 'Andriy',
+            Sername: 'Stepaniak',
+            phone: '+380680803866',
+            IT_School: 'Creator IT Academy',
+            GitHub: 'AndriyStep12',
+            twitter: '@StepAndriy'
+        }
